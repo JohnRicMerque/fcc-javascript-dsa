76a07ecfe04dab2a6ddef4e612b7e035106aab09
John Ric Merque <johnricmer@gmail.com>
2023-01-29 20:06:58 +0800

    added notes

--- palindromeChecker/palindromeChecker..js
+++ palindromeChecker/palindromeChecker..js
@@ -1,6 +1,18 @@
 function palindrome(str) {
+
+    // first obtain the array form of the string, discarding all non alphanumeric characters,
+    // also making it lowercase for checking
     const origArr = str.toLowerCase().replace(/[^0-9a-z]/gi, '').split("")
-    const reverseArr = origArr.slice().reverse()  
+
+    // obtain the reversed form of the original array, 
+    // note we used slice so that we create a copy and not mutate the original array
+    const reverseArr = origArr.slice().reverse() 
+    
+    // for all values of original array, compare it with the corresponding value in the reversed array
+    // using 'map' method this will return an array of boolean values,
+    // then use 'every' method to check if all boolean values in the array is true,
+    // if all is true then it is a palindrome and returns true, 
+    // else if at least one is false, it'll return false and thus string is not a palindrome
     return origArr.map((val, index) => val == reverseArr[index]).every(val => val === true)
   }
   
